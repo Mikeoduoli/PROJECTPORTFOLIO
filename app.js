ce70383db0f022b9af5f8cdb2a8db48a085a6d81
Obed <mikeoduoli@gmail.com>
2022-06-30 06:30:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+
+function PageTransiions(){
+    //Button click active class
+    for(let i =0; i < secBtn.length; i++){
+        secBtn[1].addEventListener('click', function (){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '')
+            this.className += 'active-btn';
+        })
+    }
+
+    //sections Active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from the other btns
+            secBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+PageTransiions();
